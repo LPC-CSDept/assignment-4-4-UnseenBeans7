32bf4ba8f0a089460d8800fe8ada90aa284713b0
UnseenBeans7 <112667610+UnseenBeans7@users.noreply.github.com>
2024-07-02 19:15:47 -0700

    assignment 4-4 completed

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ def main():
     Code Your Program here
     ########################################
     """
+    for i in range(5):
+        num = int(input("Input a number: "))
+        numbers.append(num)
+        
+    minval = numbers[0]
+    maxval = numbers[0]
+    
+    for num in numbers:
+        if num < minval:
+            minval = num
+        elif num > maxval:
+            maxval = num
 
     print(*numbers)
     print(maxval, minval)
